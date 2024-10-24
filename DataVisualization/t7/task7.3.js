4aa4a85bce8b918e1bbb3c37c791e5af53f25e29
TanSY1 <167614168+TanSY1@users.noreply.github.com>
2024-10-24 12:32:03 +0800

    Add files via upload

--- DataVisualization/t7/task7.3.js
+++ DataVisualization/t7/task7.3.js
@@ -61,6 +61,39 @@ function init() {
             return yScale(d[0]) - yScale(d[1]);
         })
         .attr("width", xScale.bandwidth());
+
+        	// select the svg area
+	var Svg = d3.select("#my_dataviz2")
+	
+	// create a list of keys
+	var keys = ["Apple", "Orange", "Grape"]
+	
+	// Usually you have a color scale in your chart already
+	var color = d3.scaleOrdinal()
+	  .domain(keys)
+	  .range(d3.schemeSet2);
+	
+	// Add one dot in the legend for each name.
+	Svg.selectAll("mydots")
+	  .data(keys)
+	  .enter()
+	  .append("circle")
+		.attr("cx", 100)
+		.attr("cy", function(d,i){ return 100 + i*25}) // 100 is where the first dot appears. 25 is the distance between dots
+		.attr("r", 7)
+		.style("fill", function(d){ return color(d)})
+	
+	// Add one dot in the legend for each name.
+	Svg.selectAll("mylabels")
+	  .data(keys)
+	  .enter()
+	  .append("text")
+		.attr("x", 120)
+		.attr("y", function(d,i){ return 100 + i*25}) // 100 is where the first dot appears. 25 is the distance between dots
+		.style("fill", function(d){ return color(d)})
+		.text(function(d){ return d})
+		.attr("text-anchor", "left")
+		.style("alignment-baseline", "middle")
 }
 
 window.onload = init;
